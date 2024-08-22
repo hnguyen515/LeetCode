8fa0f996718803235dc78074541d87344f1a279c
hnguyen515 <hnguyen515@users.noreply.github.com>
2024-08-21 17:53:49 -0700

    updating main.py to have finer print statements.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
 import LeetCode
 
-num1 = [1,2,3,0,0, 0,0]
+num1 = [1,3,5,0,0, 0,0]
 num2 = 3
-num3 = [4, 7, 8, 9]
+num3 = [2, 7, 8, 9]
 num4 = 4
+print("num1:", num1)
+print("num2:", num2)
+print("num3:", num3)
+print("num4:", num4)
 test = LeetCode.Merge()
 val = test.merge_sort(num1, num2, num3, num4)
-print("The solution is: ", val)
+print("Merging into num1: ", val)
