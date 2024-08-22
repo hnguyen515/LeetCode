946d04d9e02f362be5ea207cad47b1a88531a267
hnguyen515 <hnguyen515@users.noreply.github.com>
2024-08-21 17:25:28 -0700

    adding back main() as commented codes.

--- Merge_Sorted_Arr.py
+++ Merge_Sorted_Arr.py
@@ -33,5 +33,14 @@ val = sol.merge(num1, num2, num3, num4)
 print("The solution is: ", val)
 
 
-#if __name__ == "__main__":
-#  main()
+# def main():
+#    num1 = [1,2,3,0,0,0]
+#    num2 = 3
+#    num3 = [4, 7, 9]
+#    num4 = 3
+#    val = merge(num1, num2, num3, num4)
+#    print("The solution is: ", val)
+
+
+# if __name__ == "__main__":
+#   main()
